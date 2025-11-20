8e779e8c30bfaeec1923466d1d23ebdd3f1b61ce
Alex <sandoralecs@gmail.com>
2025-11-20 15:20:02 -0800

    Initial Commit

---
+++ techNotify/techNotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  techNotifyApp.swift
+//  techNotify
+//
+//  Created by Alex on 20/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct techNotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ techNotifyTests/techNotifyTests.swift
@@ -0,0 +1,17 @@
+//
+//  techNotifyTests.swift
+//  techNotifyTests
+//
+//  Created by Alex on 20/11/2025.
+//
+
+import Testing
+@testable import techNotify
+
+struct techNotifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
